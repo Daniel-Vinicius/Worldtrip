e600f820f8a0d01fcc993cb890f232b7e4e864d6
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-04-03 00:15:01 -0300

    Inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Worldtrip</title>
+      </Head>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
