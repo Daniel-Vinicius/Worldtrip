728ee2317656daa346064caf579b1790e8e459b2
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-04-09 20:04:14 -0300

    typecode continents

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://my-json-server.typicode.com/Daniel-Vinicius/Worldtrip",
+  baseURL:
+    "https://my-json-server.typicode.com/Daniel-Vinicius/Worldtrip/continents",
 });
 
 export default api;
